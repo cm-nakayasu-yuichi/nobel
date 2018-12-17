9e4a7cf767c39cb6e19d8893d5ff683fef048706
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-12-18 02:42:30 +0900

    [add] ベースプロジェクト追加

---
+++ Nobel/Nobel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nobel
+//
+//  Created by nakayasu.yuichi on 2018/12/18.
+//  Copyright © 2018年 yuichi.nakayasu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
