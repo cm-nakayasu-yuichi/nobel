45196a5c01c61de6c1a7d4a5da93cb077762cb96
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-12-20 00:52:37 +0900

    [add] ID生成(SHA256)

--- Sources/DataStores/Realm/RealmIdentifiedObject.swift
+++ Sources/DataStores/Realm/RealmIdentifiedObject.swift
@@ -4,6 +4,7 @@
 //
 import UIKit
 import RealmSwift
+import CommonCrypto
 
 protocol RealmIdentifiedObject {
     
@@ -17,8 +18,29 @@ protocol RealmIncrementableIdentifiedObject {
 
 extension RealmIdentifiedObject where Self: RealmSwift.Object {
     
-    static func generateId() -> String {
-        return "" // TODO:
+    static func generateId(_ number: Int = 1) -> String {
+        let seed = hashSeed(number: number)
+        return hash(string: seed).uppercased()
+    }
+    
+    private static func hashSeed(number: Int = 1) -> String {
+        return Int.random(min: 1000, max: 9999).string + number.string + Int(Date().timeIntervalSince1970).string
+    }
+    
+    private static func hash(string: String) -> String {
+        let capacity = Int(CC_SHA256_DIGEST_LENGTH)
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: capacity)
+        
+        guard let data = string.data(using: .utf8) else { return "" }
+        
+        var ret = ""
+        data.withUnsafeBytes() { (bytes: UnsafePointer<CChar>) -> () in
+            CC_SHA256(bytes, CC_LONG(data.count), buffer)
+            ret = (0..<capacity).reduce("") { str, i in
+                str + String(format: "%02x", buffer[i])
+            }
+        }
+        return ret
     }
 }
 
